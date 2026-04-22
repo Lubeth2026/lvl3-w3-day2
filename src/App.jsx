33e2b37fd7014f9cf34b921f00775c52f87691d5
Lubeth <lubethjimenez@yahoo.com>
2026-04-22 14:57:01 -0400

    Add MAP pet array demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 
 import './App.css'
+import Header from './components/Header';
+import Primary from './components/Primary';
 
 function App() {
   
 
   return (
     <>
-      
+      <Header/>
+      <Primary/>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/Primary.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+//How arrays actually work//
+//MAP for the pet array//
+//MAP looping over each item/Taking item & passing it in as a parameter//
+//MAP turns Data into Elements//
+export default function Primary() {
+  const name = "Milo";
+  const petNames = [name, "Luna", "Daisy"];
+ 
+  const jsxNames = petNames.map(petName => {
+    <p key={petName}>{petName}</p>;
+  });
+
+  return (
+    <>
+      <div>
+        <h2>Pet Names:</h2>
+        {jsxNames}
+        
+      </div>
+    </>
+  );
+}
